3272582a70922775b09723b887bba5440d5d8499
ydtg1993 <946818508@qq.com>
2026-04-06 17:20:18 +0800

    papa

--- internal/models/crawler_task.go
+++ internal/models/crawler_task.go
@@ -7,20 +7,21 @@ import (
 type TaskStatus int
 
 const (
-	TaskStatusPending    TaskStatus = 0 // 待处理
-	TaskStatusSuccess    TaskStatus = 1 // 成功
-	TaskStatusFailed     TaskStatus = 2 // 失败
-	TaskStatusProcessing TaskStatus = 3 // 处理中
+	TaskStatusPending    TaskStatus = iota // 待处理
+	TaskStatusProcessing                   // 处理中
+	TaskStatusSuccess                      // 成功
+	TaskStatusFailed                       // 失败
 )
 
 type CrawlerTask struct {
 	ID          uint       `gorm:"primarykey;comment:任务ID"`
+	PID         uint       `gorm:"index;type:int(11);comment:父级任务ID"`
 	Stage       string     `gorm:"type:varchar(50);not null;uniqueIndex:idx_stage_url,priority:1;comment:所属阶段(catalog/detail等)"`
 	URL         string     `gorm:"type:varchar(500);not null;uniqueIndex:idx_stage_url,priority:2;comment:任务URL"`
 	Title       string     `gorm:"type:text;comment:页面标题"`
-	Content     string     `gorm:"type:longtext;comment:页面HTML内容"`
+	Content     string     `gorm:"type:json;comment:页面提取内容"`
 	Retry       int        `gorm:"default:0;comment:已重试次数"`
-	Status      TaskStatus `gorm:"default:0;comment:0:待处理 1:成功 2:失败 3:处理中"`
+	Status      TaskStatus `gorm:"default:0;comment:0:待处理 1:处理中 2:成功 3:失败"`
 	Error       string     `gorm:"type:text;comment:错误信息"`
 	ScheduledAt *time.Time `gorm:"index;comment:计划执行时间"`
 	CreatedAt   time.Time  `gorm:"comment:创建时间"`
